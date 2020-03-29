752d8b4225645b6d35596552e3a20a3e363cb61a
tjrb3 <John_Bodenhamer@unigroup.com>
2020-03-29 11:22:51 -0500

    process

--- a01_recursion/main.cpp
+++ a01_recursion/main.cpp
@@ -1,6 +1,86 @@
 #include <iostream>
 
+char getChoice();
+
+
 int main() {
     std::cout << "Hello, World!!!" << std::endl;
     return 0;
 }
+
+char getChoice()
+{
+    char choice;
+    bool valid;
+
+    cout << "======== Menu ========\n"
+         << " 1.   Sum of n integers \n"
+         << " 2.   Sum of Odds or Evens in n integers \n"
+         << " 3.   Multiply by addition \n"
+         << " 4.   Determine the square root of a number \n"
+         << " 5.   Greatest Common Divisor \n"
+         << " 6.   Determine the sum of the digits of an integer \n"
+         << " 7.   Raise the power of a number \n"
+         << " 8.   Search for array elements  \n"
+         << " 9.   Quit       \n\n";
+
+    do
+    {
+        cin >> choice;
+
+        switch(choice)
+        {
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9': valid = true;
+                break;
+            default : valid = false;
+                cout << "\aInvalid choice\n"
+                     << "Please try again: ";
+                break;
+
+        }
+    }
+    while (!valid);
+
+    return choice;
+}
+
+void process()
+{
+    char choice;
+
+    do
+    {
+        choice = getChoice();
+
+        switch (choice)
+        {
+            case '1' : cout << "1 \n";
+                break;
+            case '2' : cout << "2 \n";
+                break;
+            case '3' : cout << "3 \n";
+                break;
+            case '4' : cout << "4 \n";
+                break;
+            case '5' : cout << "5 \n";
+                break;
+            case '6' : cout << "6 \n";
+                break;
+            case '7' : cout << "7 \n";
+                break;
+            case '8' : cout << "8 \n";
+                break;
+            case '9' : break;
+        }
+    }
+    while (choice != '9');
+}
+
