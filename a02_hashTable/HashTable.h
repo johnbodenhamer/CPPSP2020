b6602b058b7d95181bd3054bed633f057f9da09f
johnbodenhamer <ryanbodenhamer@gmail.com>
2020-04-04 14:39:21 -0500

    test

---
+++ a02_hashTable/HashTable.h
@@ -0,0 +1,132 @@
+// HashTable.h -- class template for a hash table using chaining
+
+#include "List.h"
+
+// client must provide a hash function with the following characteristics:
+//		1 input parameter of DT (see below), passed by const reference
+//	    returns an integer between 0 and size - 1, inclusive, where size is the
+//		number of elements in the hash table
+//		the name of the function is passed as the first parameter to the HashTable 
+//		constructor
+//		client may make as many hash functions for as many HashTables as desired
+// if a struct is used for DT, a typical use of retrieve, remove, and update would
+// be to set the key of an object and then pass the object into the function
+// retrieve, remove, and update will return false if:
+//		(1) the hash function supplied above does not return a valid index
+//	    (2) the supplied key or element cannot be found in the hash table
+// otherwise, these functions will return true if successful
+// For retrieve, remove and update functions, if you are using an object as an element, a 
+// typical use would be to set the key of the desired object to retrieve, remove, or
+// update; then pass the object into the function.
+// If an object of a struct is used for DT, the == operator must be 
+// overloaded for it; the left and right operands are both DT objects
+// the == comparison is used for finding elements, usually by key value
+//		Exceptions can be thrown in the constructor, the (default) copy constructor,
+// the (default) assignment operator, or the insert function if out of heap memory.
+
+template <class DT>
+class HashTable 
+{
+public:
+	HashTable( int (*hf)(const DT &), int);  //The constructor prototype
+	~HashTable();  //The destructor
+	HashTable(const HashTable<DT>&);  //The copy constructor prototype
+	HashTable<DT>& operator=(const HashTable<DT>&);  //The overload assignment operator
+	bool insert( const DT &); // returns true if successful; returns
+					                     // false if invalid index was returned 
+										 // from the hash function
+	bool retrieve( DT &);  // see description above class template
+	void makeEmpty( );  //The function to remove all entries
+private:
+	List<DT>* table;  //The underlying array of linked lists
+	int (*hashfunc)(const DT &); // pointer to the hash function from client
+	inline void deepCopy(const HashTable<DT>&);  //The deep copy prototype
+	int size;  //The size of the underlying array
+};
+
+// The function definitions for a hash table using chaining follows
+
+//The constructor
+template <class DT>
+HashTable<DT>::HashTable( int (*hf)(const DT &), int s )
+{
+	size=s;
+	table= new List<DT>[size];
+	hashfunc = hf;
+}
+
+//The destructor
+template <class DT>
+HashTable<DT>::~HashTable()
+{
+	makeEmpty();
+	delete [] table;
+}
+
+//The copy constructor
+template <class DT>
+HashTable<DT>::HashTable(const HashTable<DT>& original)
+{
+	deepCopy(original);
+}
+
+//The overloaded assignment operator
+template <class DT>
+HashTable<DT>& HashTable<DT>::operator=(const HashTable<DT>& original)
+{
+	if (this==&original)
+		return *this;
+	delete [] table;
+	deppCopy(original);
+	return *this;
+}
+
+//The deep copy function
+template <class DT>
+inline void HashTable<DT>::deepCopy(const HashTable<DT>& original)
+{
+	size=original.size;
+	hashfunc = original.hashfunc;
+	table= new List<DT>[size];
+	for(int i=0; i< size;i++)
+	{
+		table[i]=original.table[i];
+	}
+}
+
+//The function to insert an item into the hash table
+template <class DT>
+bool HashTable<DT>::insert( const DT & newObject )
+{
+	int location = hashfunc( newObject );
+	cout << "The calculated location in the insert function is " 
+		<< location << endl;
+	if ( location < 0 || location >= size )
+		return false;
+	table[ location ].insert( newObject ) ;
+	return true;
+}
+
+//The function to locate and retireve a selected item in the hash table
+template <class DT>
+bool HashTable<DT>::retrieve( DT & retrieved )
+{
+	int location = hashfunc( retrieved );
+	cout << "The calculated location in the retrieve function is "
+		<< location << endl;
+	if ( location < 0 || location >= size )
+		return false;
+	if ( !table[ location ].retrieve( retrieved ) )
+		return false;
+	return true;
+}
+
+//The function to remove all entries in the hash table
+template <class DT>
+void HashTable<DT>::makeEmpty( )
+{
+	for ( int i = 0; i < size; i++ )
+		table[ i ].makeEmpty( );
+}
+
+
---
+++ a02_hashTable/List.h
@@ -0,0 +1,259 @@
+
+// List.h -- class for a linked list as a data structure
+
+template <class DT>
+struct Node {
+	DT info;
+	Node<DT> *next;
+};
+
+template <class DT>
+class List
+{
+public:
+	List( );
+	List( const List<DT> & );
+	~List( );
+	List<DT> & operator =( const List<DT> & );
+	void insert( const DT &); // no current position after use
+	bool first( DT & );	  // returns first element of list in listEl
+										  // and current position is set to this element; 
+										  // if list is empty, returns false and there is
+										  // no current position; otherwise, returns true
+	inline bool getNext( DT & );	  // retrieves the next element of a linked list
+										  // beyond the last element that was retrieved
+										  // by first or getNext functions and returns
+										  // it in listEl;
+										  // current position is set to this element.
+										  // if no element exists at this position, 
+										  // getNext returns false and there is no 
+										  // current position; returns true otherwise	
+	bool find ( const DT & );  // returns true if element is found
+									      // returns false if element is not found
+										  // if found, found element becomes current
+										  // position in list; if not found, there is
+										  // no current position
+	bool retrieve( DT & );  // like find, except returns found element
+	bool isEmpty( ) const;				  // returns true if linked list is empty
+										  // returns false otherwise; current position
+										  // unchanged
+	void makeEmpty( );					  // no current position
+	static int getCollisionCounter();
+	static void resetCollisionCounter();
+	bool remove(const DT);
+private:
+	Node<DT> *start;
+	Node<DT> *current;			  // points to node at current position	
+	inline void deepCopy( const List<DT> & );
+	static int collisionCounter;
+};
+
+// List.cpp -- function definitions for the linked list data structure
+
+#include <iostream>
+
+using namespace std;
+
+template <class DT>
+List<DT>::List( )
+{
+	start = current = NULL;
+}
+
+template <class DT>
+List<DT>::List( const List<DT> & aplist )
+{
+	deepCopy( aplist );
+}
+
+template <class DT>
+List<DT>::~List( )
+{
+	makeEmpty( );
+}
+
+template <class DT>
+List<DT> & List<DT>::operator =( const List<DT> & rlist )
+{
+	if ( this == &rlist )
+		return *this;
+	makeEmpty( );
+	deepCopy( rlist );
+	return *this;
+}
+
+// inserts at the beginning of the linked list
+// no current position after use		
+template <class DT>
+void List<DT>::insert( const DT & element )
+{
+	current = NULL;
+	Node<DT> *newNode = new Node<DT>;
+	newNode->info = element;
+	newNode->next = start;
+	Node<DT> *ptr = start;
+	if(start!=NULL)
+	{
+		cout<<"There was a collision loading "<<element<<endl;
+		cout<<"It collided with "<<start->info<<endl;
+		collisionCounter++;
+	}
+	else
+		cout<<"There was no collision loading "<<element<<endl;
+	start = newNode;
+	
+}	
+
+// returns first element of list in listEl and current position is set to this element; 
+// if list is empty, returns false and there is no current position; 
+// otherwise, returns true
+template <class DT>
+bool List<DT>::first( DT & listEl )
+{
+	if ( start == NULL ) 
+		return false;
+
+	current = start;
+	listEl = start->info;
+	return true;
+}
+
+// retrieves the next element of a linked list beyond the last element that was retrieved
+// by first or getNext functions and returns it in listEl;
+// current position is set to this element.
+// if no element exists at this position, getNext returns false and there is no 
+// current position; returns true otherwise	
+template <class DT>
+inline bool List<DT>::getNext( DT & listEl )				  
+{
+	if ( current == NULL ) 
+		return false;
+	if ( current->next == NULL ) {
+		current = NULL;
+		return false;
+		}
+
+	current = current->next;
+	listEl = current->info;
+	return true;
+}
+
+
+// returns true if element is found; returns false if element is not found
+// if found, found element becomes current position in list; 
+// if not found, there is no current position
+template <class DT>
+bool List<DT>::find( const DT & element )
+{
+	int pass = 1;
+	DT item;
+	if ( !first( item ) )
+		return false;
+	do 
+	{
+		if ( item == element ) 
+			return true;
+	}
+	while ( getNext( item ) );
+
+	return false;
+}
+
+// returns true if element is found; returns false if element is not found
+// if found, found element becomes current position in list; 
+// if not found, there is no current position
+template <class DT>
+bool List<DT>::retrieve( DT & element )
+{
+	Node<DT> *ptr = start;
+	while(ptr!=NULL)
+	{
+		cout<<"There was a collision here with "<<ptr->info<<endl;
+		ptr=ptr->next;
+	}
+	if ( !find( element ) )
+		return false;
+	element = current->info;
+	return true;
+}
+
+template <class DT>
+bool List<DT>::isEmpty( ) const				  
+{
+	return start == NULL;
+}
+
+template <class DT>
+void List<DT>::makeEmpty( )					  
+{
+	while ( start != NULL ) {
+		current = start;
+		start = start->next;
+		delete current;
+	}
+
+	current = NULL;
+}
+
+template <class DT>
+inline void List<DT>::deepCopy( const List<DT> & original )
+{
+	start = current = NULL;
+	if ( original.start == NULL )
+		return;
+	Node<DT> *copyptr = start = new Node<DT>;
+	Node<DT> *originalptr = original.start;
+	copyptr->info = originalptr->info;
+	if ( originalptr == original.current )
+		current = copyptr;
+	while ( originalptr->next != NULL ) {
+		originalptr = originalptr->next;
+		copyptr->next = new Node<DT>;
+		copyptr = copyptr->next;
+		copyptr->info = originalptr->info;
+		if ( originalptr == original.current )
+			current = copyptr;
+	}
+	copyptr->next = NULL;
+}
+template <class DT>
+bool List<DT>::remove(const DT element)
+{
+	Node<DT> *ptr = start;
+	bool found = false;
+	if (find(element))
+	{
+		if (ptr->info == element)
+		{
+			start = start->next;
+			delete ptr;
+		}
+		else
+		{
+			while (!(ptr->next->info == element))
+				ptr = ptr->next;
+			Node<DT> *ptr2 = ptr->next;
+			ptr->next = ptr2->next;
+			delete ptr2;
+		}
+		collisionCounter--;
+		found = true;
+	}
+	return found;
+}
+
+template <class DT>
+int List<DT>::getCollisionCounter()
+{
+	return collisionCounter;
+}
+
+template <class DT>
+void List<DT>::resetCollisionCounter()
+{
+	collisionCounter=0;
+}
+
+template <class DT>
+int List<DT>::collisionCounter=0;
+
