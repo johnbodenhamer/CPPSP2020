3e9b509f77e2f69b233808a4a06cfe480bf4761b
johnbodenhamer <ryanbodenhamer@gmail.com>
2020-04-04 15:27:09 -0500

    added file read

--- a02_hashTable/main.cpp
+++ a02_hashTable/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <string>
+#include<istream>
+#include<fstream>
+using namespace std;
+
+#include "List.h"
+#include "HashTable.h"
+
+struct Product {
+    string upc;
+    string productDescription;
+    double productCost;
+    double productInventory;
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Product p;
+    string fileName = "Inventory.csv";
+    ifstream fileIn;
+    fileIn.open(fileName);
+    string cStr;
+
+    getline(fileIn, cStr);
+    while(getline(fileIn, p.upc, ','))
+    {
+        getline(fileIn, cStr, ',');
+        p.productDescription = cStr;
+        getline(fileIn, cStr, ',');
+        p.productCost = stod(cStr);
+        getline(fileIn, cStr, ',');
+        p.productInventory = stod(cStr);
+    }
+
     return 0;
 }
