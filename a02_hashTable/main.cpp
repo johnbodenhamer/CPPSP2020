7f825d8f51d1553b53037c1098d3e700bf8424c6
johnbodenhamer <ryanbodenhamer@gmail.com>
2020-04-04 16:09:08 -0500

    what i ahve so far

--- a02_hashTable/main.cpp
+++ a02_hashTable/main.cpp
@@ -4,7 +4,6 @@
 #include<fstream>
 using namespace std;
 
-#include "List.h"
 #include "HashTable.h"
 
 struct Product {
@@ -14,14 +13,17 @@ struct Product {
     double productInventory;
 };
 
+int hf1(const Product &);
+const int SIZE = 6;
+
 int main() {
+    HashTable<Product> productTable(hf1, SIZE);
     Product p;
     string fileName = "Inventory.csv";
     ifstream fileIn;
     fileIn.open(fileName);
     string cStr;
 
-    getline(fileIn, cStr);
     while(getline(fileIn, p.upc, ','))
     {
         getline(fileIn, cStr, ',');
@@ -30,7 +32,21 @@ int main() {
         p.productCost = stod(cStr);
         getline(fileIn, cStr, ',');
         p.productInventory = stod(cStr);
+
+//        productTable.insert(p);
+
+        cout << p.productDescription << endl;
     }
+
+    fileIn.close();
 
     return 0;
 }
+
+int hf1(const Product & product)
+{
+    int  upcInt = stoi(product.upc);
+    int index = int(0.618033 * upcInt) % SIZE;
+    return index;
+}
+
